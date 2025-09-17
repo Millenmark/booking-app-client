224f4982ad3c526d0beda18cf00c583fae0d38ad
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-09-17 18:14:45 +0800

    fix: error on build

--- apps/customer/src/utils/aos.tsx
+++ apps/customer/src/utils/aos.tsx
@@ -1,20 +1,16 @@
-'use client'
-import { useEffect } from "react";
-import AOS from "aos"
-import 'aos/dist/aos.css';
+"use client";
+import { useEffect, ReactNode } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const Aoscompo = ({children}:any) => {
-    useEffect(() => {
-        AOS.init({
-            duration: 800,
-            once: false,
-        })
-    }, [])
-  return (
-    <div>
-      {children}
-    </div>
-  )
-}
+const Aoscompo = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: false,
+    });
+  }, []);
+  return <div>{children}</div>;
+};
 
-export default Aoscompo
+export default Aoscompo;
