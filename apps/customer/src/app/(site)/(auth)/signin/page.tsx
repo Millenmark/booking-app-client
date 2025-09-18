08d3e716309287318208095481aecf0bf5461118
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-09-18 23:44:53 +0800

    mod: remove breadcrumb

--- apps/customer/src/app/(site)/(auth)/signin/page.tsx
+++ apps/customer/src/app/(site)/(auth)/signin/page.tsx
@@ -1,17 +1,13 @@
 import Signin from "@/app/components/Auth/SignIn";
-import Breadcrumb from "@/app/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "Sign In | Property",
+  title: "Sign In | Property",
 };
 
 const SigninPage = () => {
   return (
     <>
-      <Breadcrumb pageName="Sign In Page" />
-
       <Signin />
     </>
   );
